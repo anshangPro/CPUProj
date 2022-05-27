c44cc794e6260731eb8741773f46948662a85e8f
dgy <ntdgy2001@gmail.com>
2022-05-28 02:47:41 +0800

    finish task1 011

--- CPUproj.srcs/code/test1.c
+++ CPUproj.srcs/code/test1.c
@@ -70,7 +70,7 @@ int main() {
         }
       }
     }
-  }else if(sw[23] == 0 && sw[22] == 1 && sw[21] == 0){
+  } else if (sw[23] == 0 && sw[22] == 1 && sw[21] == 0) {
     while (sw[20] == 0) {
     }
     while (sw[20] == 1) {
@@ -86,7 +86,27 @@ int main() {
       }
     }
     for (int i = 0; i < 16; i++) {
-      if(data1[i] & data2[i]){
+      if (data1[i] & data2[i]) {
+        *led24 = *led24 | (~(1 << i));
+      }
+    }
+  } else if (sw[23] == 0 && sw[22] == 1 && sw[21] == 1) {
+    while (sw[20] == 0) {
+    }
+    while (sw[20] == 1) {
+      for (int i = 0; i < 16; i++) {
+        data1[i] = sw[i];
+      }
+    }
+    while (sw[20] == 0) {
+    }
+    while (sw[20] == 1) {
+      for (int i = 0; i < 16; i++) {
+        data2[i] = sw[i];
+      }
+    }
+    for (int i = 0; i < 16; i++) {
+      if (data1[i] | data2[i]) {
         *led24 = *led24 | (~(1 << i));
       }
     }
